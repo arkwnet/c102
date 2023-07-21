bbc1832097b5086b13cdf7fa2b7ed93faf05ae35
Sora Arakawa <mail@arkw.net>
2023-07-21 17:00:10 +0900

    dl-backend: Return json

--- dl-backend/app/src/main.rs
+++ dl-backend/app/src/main.rs
@@ -1,16 +1,18 @@
-use actix_web::{get, App, HttpServer, Responder};
+use actix_web::{get, App, HttpServer, HttpResponse};
 
 #[get("/")]
-async fn index() -> impl Responder {
-    "Hello world!"
+async fn index() -> HttpResponse {
+  HttpResponse::Ok()
+  .content_type("application/json")
+  .body(r#"{"result": true, "data": "Hello world!"}"#)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new().service(index)
-    })
-    .bind(("app", 8080))?
-    .run()
-    .await
+  HttpServer::new(|| {
+    App::new().service(index)
+  })
+  .bind(("app", 8080))?
+  .run()
+  .await
 }
