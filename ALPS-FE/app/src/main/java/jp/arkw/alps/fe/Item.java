0523ab398271de293d377d3ffb135535fbaf2726
Sora Arakawa <mail@arkw.net>
2023-07-23 09:47:15 +0900

    ALPS-FE: Add listViewItem touch event

--- ALPS-FE/app/src/main/java/jp/arkw/alps/fe/Item.java
+++ ALPS-FE/app/src/main/java/jp/arkw/alps/fe/Item.java
@@ -4,11 +4,13 @@ class Item {
     String name;
     int price;
     int image;
+    int quantity;
 
     Item(String name, int price, int image) {
         this.name = name;
         this.price = price;
         this.image = image;
+        this.quantity = 0;
     }
 
     public String getName() {
@@ -22,4 +24,8 @@ class Item {
     public int getImage() {
         return this.image;
     }
+
+    public int getQuantity() { return this.quantity; }
+
+    public void setQuantity(int quantity) { this.quantity = quantity; }
 }
--- ALPS-FE/app/src/main/java/jp/arkw/alps/fe/MainActivity.java
+++ ALPS-FE/app/src/main/java/jp/arkw/alps/fe/MainActivity.java
@@ -3,6 +3,8 @@ package jp.arkw.alps.fe;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
@@ -13,6 +15,7 @@ import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     private ArrayList<Item> items = new ArrayList<Item>();
+    private ArrayList<Map<String, String>> listSelect = new ArrayList<>();
     private int total = 0;
 
     @Override
@@ -25,15 +28,15 @@ public class MainActivity extends AppCompatActivity {
         items.add(new Item("ひらがないれーす", 100, R.drawable.gamecd));
         items.add(new Item("ハコ単", 100, R.drawable.gamecd));
 
-        ArrayList<Map<String, String>> listSelect = new ArrayList<>();
+        SimpleAdapter simpleAdapter = new SimpleAdapter(
+                this,
+                listSelect,
+                android.R.layout.simple_list_item_2,
+                new String[] {"name", "detail"},
+                new int[] {android.R.id.text1, android.R.id.text2}
+        );
         ListView listViewSelect = findViewById(R.id.list_select);
-        listViewSelect.setAdapter(new SimpleAdapter(
-            this,
-            listSelect,
-            android.R.layout.simple_list_item_2,
-            new String[] {"name", "detail"},
-            new int[] {android.R.id.text1, android.R.id.text2}
-        ));
+        listViewSelect.setAdapter(simpleAdapter);
 
         ArrayList<Map<String, Object>> listItem = new ArrayList<>();
         for (int i = 0; i < items.size(); i++) {
@@ -51,5 +54,28 @@ public class MainActivity extends AppCompatActivity {
             new String[] {"name", "detail", "image"},
             new int[] {R.id.name, R.id.detail, R.id.image}
         ));
+
+        listViewItem.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int index, long l) {
+                items.get(index).setQuantity(items.get(index).getQuantity() + 1);
+                updateListSelect();
+                simpleAdapter.notifyDataSetChanged();
+            }
+        });
+    }
+
+    private void updateListSelect() {
+        listSelect.clear();
+        for (int i = 0; i < items.size(); i++) {
+            Item item = items.get(i);
+            if (item.getQuantity() >= 1) {
+                Map<String, String> map = new HashMap<>();
+                int subtotal = item.getPrice() * item.getQuantity();
+                map.put("name", item.getName());
+                map.put("detail", "￥ " + item.getPrice() + " × " + item.getQuantity() + " = ￥ " + subtotal);
+                listSelect.add(map);
+            }
+        }
     }
 }
