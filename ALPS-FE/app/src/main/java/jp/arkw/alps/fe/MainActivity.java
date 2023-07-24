700d2c1fa6527bb3ba66d0c5b6b245de112ae33a
Sora Arakawa <mail@arkw.net>
2023-07-24 10:53:55 +0900

    ALPS-FE: Add business card printing feature

--- ALPS-FE/app/src/main/java/jp/arkw/alps/fe/MainActivity.java
+++ ALPS-FE/app/src/main/java/jp/arkw/alps/fe/MainActivity.java
@@ -2,18 +2,35 @@ package jp.arkw.alps.fe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.RemoteException;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
+import com.sunmi.peripheral.printer.InnerPrinterCallback;
+import com.sunmi.peripheral.printer.InnerPrinterException;
+import com.sunmi.peripheral.printer.InnerPrinterManager;
+import com.sunmi.peripheral.printer.InnerResultCallback;
+import com.sunmi.peripheral.printer.SunmiPrinterService;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private SunmiPrinterService sunmiPrinterService;
+    public static int NoSunmiPrinter = 0x00000000;
+    public static int CheckSunmiPrinter = 0x00000001;
+    public static int FoundSunmiPrinter = 0x00000002;
+    public static int LostSunmiPrinter = 0x00000003;
+    public int sunmiPrinter = CheckSunmiPrinter;
+
     private ArrayList<Item> items = new ArrayList<>();
     private ArrayList<Map<String, String>> listSelect = new ArrayList<>();
     private SimpleAdapter simpleAdapter;
@@ -24,6 +41,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initSunmiPrinterService(this);
 
         items.add(new Item("ひらがないれーす", 100, R.drawable.gamecd));
         items.add(new Item("ハコ単", 100, R.drawable.gamecd));
@@ -63,13 +81,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         });
 
+        findViewById(R.id.button_card).setOnClickListener(this);
         findViewById(R.id.button_clear).setOnClickListener(this);
         update();
     }
 
     @Override
     public void onClick(View v) {
-        if (v.getId() == R.id.button_clear) {
+        if (v.getId() == R.id.button_card) {
+            printImage(BitmapFactory.decodeResource(getResources(), R.drawable.card));
+            feedPaper();
+        } else if (v.getId() == R.id.button_clear) {
             for (int i = 0; i < items.size(); i++) {
                 items.get(i).setQuantity(0);
             }
@@ -94,5 +116,81 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
         simpleAdapter.notifyDataSetChanged();
         textView.setText("合計: ￥ " + total);
+    }
+
+    private void printImage(Bitmap bitmap) {
+        try {
+            sunmiPrinterService.printBitmap(bitmap, new InnerResultCallback() {
+                @Override
+                public void onRunResult(boolean isSuccess) throws RemoteException {
+                }
+                @Override
+                public void onReturnString(String result) throws RemoteException {
+                }
+                @Override
+                public void onRaiseException(int code, String msg) throws RemoteException {
+                }
+                @Override
+                public void onPrintResult(int code, String msg) throws RemoteException {
+                }
+            });
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        };
+    }
+
+    private void feedPaper() {
+        try {
+            sunmiPrinterService.lineWrap(5, new InnerResultCallback() {
+                @Override
+                public void onRunResult(boolean isSuccess) throws RemoteException {
+                }
+                @Override
+                public void onReturnString(String result) throws RemoteException {
+                }
+                @Override
+                public void onRaiseException(int code, String msg) throws RemoteException {
+                }
+                @Override
+                public void onPrintResult(int code, String msg) throws RemoteException {
+                }
+            });
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        };
+    }
+
+    private InnerPrinterCallback innerPrinterCallback = new InnerPrinterCallback() {
+        @Override
+        protected void onConnected(SunmiPrinterService service) {
+            sunmiPrinterService = service;
+            checkSunmiPrinterService(service);
+        }
+        @Override
+        protected void onDisconnected() {
+            sunmiPrinterService = null;
+            sunmiPrinter = LostSunmiPrinter;
+        }
+    };
+
+    private void checkSunmiPrinterService(SunmiPrinterService service) {
+        boolean ret = false;
+        try {
+            ret = InnerPrinterManager.getInstance().hasPrinter(service);
+        } catch (InnerPrinterException e) {
+            e.printStackTrace();
+        }
+        sunmiPrinter = ret?FoundSunmiPrinter:NoSunmiPrinter;
+    }
+
+    public void initSunmiPrinterService(Context context) {
+        try {
+            boolean ret =  InnerPrinterManager.getInstance().bindService(context, innerPrinterCallback);
+            if (!ret) {
+                sunmiPrinter = NoSunmiPrinter;
+            }
+        } catch (InnerPrinterException e) {
+            e.printStackTrace();
+        }
     }
 }
