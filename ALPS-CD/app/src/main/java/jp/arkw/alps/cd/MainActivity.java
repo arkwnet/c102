2ef90fb97e653a311da2a030c90f57467d38a5d1
Sora Arakawa <mail@arkw.net>
2023-08-11 22:53:00 +0900

    ALPS-CD: Create icon

--- ALPS-CD/app/src/main/java/jp/arkw/alps/cd/MainActivity.java
+++ ALPS-CD/app/src/main/java/jp/arkw/alps/cd/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -44,6 +45,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         linearLayout = findViewById(R.id.linear_layout);
         textUpperLeft = findViewById(R.id.text_upper_left);
         textUpperRight = findViewById(R.id.text_upper_right);
